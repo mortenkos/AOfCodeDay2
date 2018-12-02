5a54fe594dc525a9ce122c28829d21b3fc82e946
Morten Kose <morten.kose@gmail.com>
2018-12-02 14:19:14 +0200

    solution part1

---
+++ part1.js
@@ -0,0 +1,60 @@
+const listOfID = ["nkucgflathzwwijtrevymbdplq","nkucgflathzwsidxrlvymbdpiq","nkucgjlathqwsijxrevymbypoq","nkucgflarrzwsmjxrevymbdpoq","nkucgflzthtwsijxrevymbdpjq","nkucgflahhzwskjxrevymbgpoq","bkycgflathzwsijxrsvymbdpoq","nkucgflathzwsijxdevymbdmog","nkucgflaehzwsmjxrevymbdpow","nkucgflathzwsijxrevwmbdnbq","nkucgflathzssijxrevynbdqoq","ngucgflathzwsijxsevymndpoq","nfucgflathzvsijxrevymbspoq","nkucgflwthzwsijxreeymbdpkq","nkucgflpthzwsijxrevdmbdpoe","nkungflatuzwsijurevymbdpoq","nkucgflathzwsiqxrevyybdpom","nkucgflathzwsicxrevtmbtpoq","nkucgfladhzwsijxreuymbdboq","nkumgflathznsijxzevymbdpoq","nkuagflethzwsijxrqvymbdpoq","nkucgflatozwhijxrevymbdpkq","nkuggflathzwsijxrejymbdpob","nkucgflathzwlijxrqvambdpoq","hkucnflathzasijxrevymbdpoq","nkuigflathzwsirxrevymbdooq","nkucgflatezwsijxwetymbdpoq","nkucmflavhbwsijxrevymbdpoq","nkucgflathzwssjxrevytbmpoq","nkucgflmthzwsvjxrevymbdpgq","nkucgtlathzwsijcrevymbjpoq","nkucgflathfwsfjxrevymbdpsq","nkucgflathjwsijxrwvymbdpok","nkucgeldthzwsijxrevymqdpoq","nkutgcpathzwsijxrevymbdpoq","nkucgflaehzmsijxrevymydpoq","mkucdflathzwsvjxrevymbdpoq","nkucgflathzwsijxtevymidpfq","nkucgfllthzwsijirevlmbdpoq","nkucgfuathzwsijxrevymbqpou","nkufgflathzwsijxrgvymbdpor","nkuygflatrzwsijxrevymbdpoo","nkunoflathzwsijxrevyabdpoq","nksogflathzwsijxrevymbdpeq","nkucgflathzwciexrevymbdhoq","nkucgfnathzwsijxdevyobdpoq","nkudgflazhzwsijxrevymbmpoq","nkucgylathzwscjxrevymbdaoq","nkucgflqthzisijxrerymbdpoq","nkucgxlathzwsijxgebymbdpoq","nkucgflathzssijxrwvymadpoq","nkucgflathzwsijxrvvymbdloi","nkucaflathzwskjxzevymbdpoq","wkuchflathzwsijxrevymbdioq","nkucgilathzwsijxrgvympdpoq","nkubgflaohzwsijxrevymbnpoq","nkucgwoathzwnijxrevymbdpoq","nkprgflathzwsijxrevywbdpoq","nkucgflatlzwsijxievymzdpoq","nkucgflathzwsijxrevymbmdow","nkucgzlathzwsitlrevymbdpoq","nkubgfladhzwsijxrevymbdpsq","nkucgflathzwsijxrzvyibdroq","wkucgflathznsijqrevymbdpoq","nkupgilathzwsijxrnvymbdpoq","nkucgflathzwwijnrevymgdpoq","nkucgflathjwsijxrewymbopoq","mkwcdflathzwsvjxrevymbdpoq","nkucgflathzwsujxoevymbdboq","nkucvflathzwsojxrevymzdpoq","nkocgflabhzwsijxrevyebdpoq","skuciflpthzwsijxrevymbdpoq","nkuxgflathzrsijxrevymbupoq","nkucgblathzwsezxrevymbdpoq","nkucgflathzwsijxrevymbvtop","nkucdflathzwsiaxrefymbdpoq","nkucgflathzwsijxzevkmbdpmq","nkucgflarhzwsijroevymbdpoq","nkuccflathzwsinxrevymsdpoq","nkucgflathzwsijxregymidsoq","nkucgflathnwsijxrvvumbdpoq","nkucgfaathzwssjxrevymbdwoq","nkucgflothzwsijxrevymbdloz","naucgulathzwsijxremymbdpoq","nkucgflaqhzwsijxrevymbdnqq","wkucgflatrzwsijxrevymbdpof","nvucgflaehzwyijxrevymbdpoq","nkusaflaahzwsijxrevymbdpoq","nkucgfkathzwsijxrevymbdbfq","nkucgfkathzwsijrrevymodpoq","nkuegflathzwsijxrrvbmbdpoq","ykucgflathzwsijfrlvymbdpoq","nkucgflathzrsujxrevymbdkoq","nkuclflatsowsijxrevymbdpoq","nkucgflathzwsgjxrqvymbdpor","ekucgflathzwsijorevlmbdpoq","nkucgflathizsijgrevymbdpoq","nkucgfoathzksijbrevymbdpoq","nkucgflachzwsijxrevymbupoa","nkuhgflathzwsijxravylbdpoq","nkncgflithzwsijnrevymbdpoq","nvucgjlathzjsijxrevymbdpoq","nhucgflathzwsijxrtvymbtpoq","akucgflathzwhijxrevymbdpor","nkucgflatozisijxrvvymbdpoq","nkucgzlatgzwsijxrevymbepoq","nkjcgflqthmwsijxrevymbdpoq","nkucgflaohzosijxrhvymbdpoq","ntucgflatrzwsijxrevymbdpol","nkucgflathzwsijkriqymbdpoq","nkuggflathzwsijmrevymbdpvq","nkucgflpmhzwsmjxrevymbdpoq","okucgflathzwsijxrevgmbdsoq","nkucgflaehzwkijxrevymwdpoq","zkucgfllthzwsijxrevymbdpod","nkicgflathzasijxfevymbdpoq","kkucgfhathzwsijxaevymbdpoq","nkucqflsthzwsijxrevyjbdpoq","nkucgflaghzwsijxoevykbdpoq","nkucgflaohzwsljxryvymbdpoq","bkucfflathzwsijxrexymbdpoq","nkucnflathzwsbjxrpvymbdpoq","nkucjflatlzwsijxrevymqdpoq","nkucgflathzwsijsyevyxbdpoq","nkwcgflathzosijxqevymbdpoq","nkucgslathzesijxievymbdpoq","nkuciflauhzwsiaxrevymbdpoq","nkucgflathzwsiwxreeymbdwoq","nkucgblatwzwsijxkevymbdpoq","njucgfkathzwsijxrevymbvpoq","nkucgfladhzwsijfrevyibdpoq","nkukgflathzwsijprenymbdpoq","nkucgflathzwsijxrchymbupoq","nkucgfeathzwsitxaevymbdpoq","nkufjflathzwsijxresymbdpoq","nkuggflatlzwsijxrevymbdpoa","nkucgflsthnwsijxrevumbdpoq","nkuceflathzwsnjxrevymbmpoq","nkucgflabhzwsijxrevymblplq","nkucgfmathzwsijxrevdybdpoq","niuvgflathzwsijxrcvymbdpoq","nkscgflathzwsijxrevyzbdooq","nkucgflatszwsbjxrevymbgpoq","nkucgflazhzwsijxcevymzdpoq","nkucgflathzwsfjqrevymbdpxq","nkucgflathcwsijxrrvymbdroq","nkurgflathzwsijxrepymzdpoq","nlucgflathzwrijxrevdmbdpoq","kkucgflkthzwswjxrevymbdpoq","nktcgflathzwgijxrevbmbdpoq","nbucgfiathzwsijxreyymbdpoq","lkucgflathswsijxrevymbdpxq","ntucgflathzwswrxrevymbdpoq","nkscgflathzwssjxravymbdpoq","nuocgflathzwsijxrevyebdpoq","nbucgllathzwsijxregymbdpoq","ckucbflathzwsijxrelymbdpoq","nkucgflathzwsijxremymbqpor","nkgcgfljthzwsijkrevymbdpoq","nkdcgflashzwsijxrjvymbdpoq","nkecgflathzwsijxuevumbdpoq","njucgflatpfwsijxrevymbdpoq","nkucgwlathzjsijxrevymbzpoq","nkucgfxathzqsijxrenymbdpoq","dkfcgflathzwsijxrevymbdtoq","nkupgfhathzwsijxrevymbjpoq","nkucgflathzwsjjxrevymldooq","pkucgfbathhwsijxrevymbdpoq","nkuciflayhzwsijxrevymbdpfq","nkucpfdathzwsajxrevymbdpoq","ykucgflathdwsijzrevymbdpoq","nkucgwlstnzwsijxrevymbdpoq","nkucwfzazhzwsijxrevymbdpoq","nkucgflatczwssjxretymbdpoq","nkucgflathzwsijpreaymxdpoq","ntucgflathzwsijxrepymvdpoq","nkucgqlathzdsijxrevymbopoq","nkucgflathzusijxfevymbdptq","nkocgflathzwdijxrevymbipoq","nklcgflatgzwsijxrevymbdsoq","nkucgflathzwsgjxgevymbopoq","nkucgflathzwuijxreaymbdyoq","nkucgwlathzwsvjxrevymbdpos","nkucrflathzwliqxrevymbdpoq","nkucgflathzxsijxievysbdpoq","nkufgolhthzwsijxrevymbdpoq","niucgflathzwsiixrevyabdpoq","nkucgflathzhsijxrevymbdyuq","nkucgqlathzwsijxreaymbdpob","nzucgflathzesijxrevymwdpoq","nkucgflatlzwsirxrevymmdpoq","nkucgfxavhzwsijxrevymbwpoq","nkucgflathswsijxrevvmbdpoe","nkucgfgethzwsrjxrevymbdpoq","nkucgzlayhzwsinxrevymbdpoq","nkucgflwthzwsiyxrevymbdpdq","nkucgflpthzwsijxrezombdpoq","nkurgflathdwsijxuevymbdpoq","nkjcgflathzwsijxrevkmbdpoc","nkucmflatuzwsijxrevmmbdpoq","nkucgfldthzwsijxrevevbdpoq","nkucgflatrzgsijxrevambdpoq","nkicgflathzwsijxrevymhdhoq","nkbcgflathzwsijxrevymxdpos","nkucgflatfzwsijxrevymwdqoq","hkucgflaqwzwsijxrevymbdpoq","nkjcgflathzvsijxrevyjbdpoq","niucgflathzwsijxrezymbdpob","ynucgflathzwsijxremymbdpoq","nkubgflathzwsijxrhvymldpoq","nkucqflrthzesijxrevymbdpoq","nkucgulathzwsijxrevyubdioq","nkuczflathzwsijxaebymbdpoq","nkucgfldthzwsibxrevymrdpoq","nkucgflatwzdsijxrevymsdpoq","nkncgffathzwsijxrejymbdpoq","nkucgflathzqsijxrevxmodpoq","nkucgflathwwsijqrevymbipoq","nkucgflathzwhajxrebymbdpoq","gkucgflathzwsijxreirmbdpoq","nkucgflathzesijzravymbdpoq","nkucgflaghzwsijxrerymbdplq","wkucgflathxwgijxrevymbdpoq","nkucgfljthfwsijxrevymbdpfq","nkucgflathwwsimxrevymbdpjq","nkucgdlachzwsijxrevymmdpoq","njucgclathzwsiixrevymbdpoq","nkucgflatdzwsijxrevymzrpoq","nkuckflatvzcsijxrevymbdpoq","nkucgflathzhsijxrevqmbkpoq","nkucqflathzjsijvrevymbdpoq","nkucgftathzwsijxrevympdpoi","nvucgflatmzwsijxrevymbdpsq","nkocgflathznsijxrevymbdphq","mkgcgflathzwsijxrevymbdpvq","nkucnflathzwsijbrevymbdcoq","nkucgflathzwsijsrevymsdgoq","nkuckflatxzwsiwxrevymbdpoq","nkucyflathzwsijxrehcmbdpoq","nkurgflajhzwsijxrevkmbdpoq","wkucgflathzwsijxrfvymbapoq","nkucgflathzwsijxaekymbdpon","nkucgfkathywsijxrevymbdpsq","nkucgflathzwsijxaexcmbdpoq","nkucgflathzwsijxrevymddhox","nkucgflathzwgijxrevymydooq","nkycqflathzwsijxrezymbdpoq","nkucgflathwwsijxrevymbspsq","nkucgflatpzwssjfrevymbdpoq","nkwcgflhthzwsijxrevcmbdpoq"];
+
+
+const idCheckSum = (arr) => {
+  let withTwoLetters = 0;
+  let withThreeLetters = 0;
+  const dublicates = arr.map(id => findRepeatedChars(id));
+
+  dublicates.forEach(dArr => {
+    
+    if(charCheck(dArr) === 'both') { 
+      withTwoLetters += 1;
+      withThreeLetters += 1;
+    }
+    if(charCheck(dArr) === 'two') {
+      withTwoLetters += 1;
+    }
+    if(charCheck(dArr) === 'three') {
+      withThreeLetters += 1;
+    }
+  })    
+  return withThreeLetters * withTwoLetters;
+};
+
+function findRepeatedChars(str){
+  const result = [];
+  const strToArr = str.split("").sort().join("").match(/(.)\1+/g);
+  
+  if (strToArr != null) {
+    strToArr.forEach((el) => {
+      result.push([el]);
+    });
+  }
+  return result;
+}
+
+function charCheck(arr) {
+  let two = false;
+  let three = false;
+  for(let i = 0; i<arr.length; i++){
+    const length = arr[i].toString().length;
+    if (length === 2) {
+      two = true;
+    }
+    if (length === 3) {
+      three = true;
+    }
+  }
+  if (two && three) {
+    return 'both';
+  }else if (two) {
+    return 'two';
+  }else if (three) {
+    return 'three';
+  }
+}
+
+idCheckSum(listOfID); //Part 1 answer
+
+
